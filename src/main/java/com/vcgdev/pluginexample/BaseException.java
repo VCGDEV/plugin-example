caf889713f55c7f2f138fdaa4caa49b0d395aa43
Victor de la Cruz <vcg_cruz@hotmail.com>
2018-11-28 22:15:59 -0600

    Initial commit

---
+++ src/main/java/com/vcgdev/pluginexample/BaseException.java
@@ -0,0 +1,18 @@
+package com.vcgdev.pluginexample;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Data
+@ToString
+public class BaseException extends Exception {
+    private HttpStatus status;
+    private String message;
+
+    public BaseException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+        this.status = HttpStatus.NOT_FOUND;
+    }
+}
